5a53fdf3a097aabcc2dce2d793a06359120e881d
Teh Yi Jia <91209754+Jiffredteh@users.noreply.github.com>
2022-04-21 17:59:27 +0800

    Update Machine_learning.py

--- Interface/Machine_learning.py
+++ Interface/Machine_learning.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 from Config import util
 
 def logistic_regression_model(X_train, y_train, X_test, y_test):
