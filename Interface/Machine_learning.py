eda6b417db7bfafcdb11d2621b87cd32518160b6
Teh Yi Jia <91209754+Jiffredteh@users.noreply.github.com>
2022-04-21 18:03:47 +0800

    Update Machine_learning.py

--- Interface/Machine_learning.py
+++ Interface/Machine_learning.py
@@ -1,17 +1,16 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
 from Config import util
 
 def logistic_regression_model(X_train, y_train, X_test, y_test):
     LRmodel = LogisticRegression(solver='lbfgs',max_iter=250)
     util.machine_learning_model(LRmodel,X_train, y_train, X_test, y_test, "Logistic Regression Model")
 
-
 def naive_bayes_model(X_train, y_train, X_test, y_test):
     NaiveBayes = MultinomialNB()
     util.machine_learning_model(NaiveBayes,X_train, y_train, X_test, y_test, "Multinomial Naive Bayes Classifier")
     
-def SGD_Classifier_model(X_train, y_train, X_test, y_test):
-    SGDClassifier_model = SGDClassifier()
-    util.machine_learning_model(SGDClassifier_model,X_train, y_train, X_test, y_test, "SGD Classifier")
+def Decision_tree_model(X_train, y_train, X_test, y_test):
+    Decision_tree = DecisionTreeClassifier()
+    util.machine_learning_model(Decision_tree,X_train, y_train, X_test, y_test, "SGD Classifier")
